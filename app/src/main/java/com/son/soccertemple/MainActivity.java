c14c118af49b9d65b64def915db24a76d0ffc503
Dat Nguyen <vghsdat@gmail.com>
2021-05-17 14:01:31 +0700

    Resize images Update history Update layout Update sound, animation

--- app/src/main/java/com/son/soccertemple/MainActivity.java
+++ app/src/main/java/com/son/soccertemple/MainActivity.java
@@ -3,35 +3,149 @@ package com.son.soccertemple;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
+import android.transition.Fade;
 import android.view.View;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.AnimationUtils;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.TranslateAnimation;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import static com.son.soccertemple.R.raw.drump;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnStart, btnExit;
+    Button btnStart, btnHighscore, btnHistory, btnExit;
+    TextView text1, text2;
+    public MediaPlayer themeSong, ring;
+    Handler handler = new Handler();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnStart = (Button)findViewById(R.id.BtnTest);
-        btnExit = (Button)findViewById(R.id.BtnExit);
+        Mapping();
+        setAnimationText();
+
+        themeSong = MediaPlayer.create(this, drump);
+        themeSong.setLooping(true);
+
+
+        themeSong.start();
+
+        final Animation[] fadeOutOnClick = new Animation[1];
+        final Animation[] fadeInOnClick = new Animation[1];
 
         btnStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent startGame = new Intent(MainActivity.this, activity_setup.class);
-                startActivity(startGame);
+                Intent game = new Intent(MainActivity.this, activity_setup.class);
+                fadeOutOnClick[0] = AnimationUtils.loadAnimation(MainActivity.this,R.anim.fadeout);
+                fadeInOnClick[0] = AnimationUtils.loadAnimation(MainActivity.this,R.anim.fadein);
+
+                btnStart.startAnimation(fadeOutOnClick[0]);
+                startActivity(game);
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+                btnStart.startAnimation(fadeInOnClick[0]);
+            }
+        });
+
+        btnHighscore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent game = new Intent(MainActivity.this, activity_highscore.class);
+                fadeOutOnClick[0] = AnimationUtils.loadAnimation(MainActivity.this,R.anim.fadeout);
+                fadeInOnClick[0] = AnimationUtils.loadAnimation(MainActivity.this,R.anim.fadein);
+
+                btnHighscore.startAnimation(fadeOutOnClick[0]);
+                startActivity(game);
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+                btnHighscore.startAnimation(fadeInOnClick[0]);
+            }
+        });
+
+        btnHistory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent history = new Intent(MainActivity.this, activity_history.class);
+                fadeOutOnClick[0] = AnimationUtils.loadAnimation(MainActivity.this,R.anim.fadeout);
+                fadeInOnClick[0] = AnimationUtils.loadAnimation(MainActivity.this,R.anim.fadein);
+
+                btnHistory.startAnimation(fadeOutOnClick[0]);
+                startActivity(history);
+                btnHistory.startAnimation(fadeInOnClick[0]);
             }
         });
 
         btnExit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                fadeOutOnClick[0] = AnimationUtils.loadAnimation(MainActivity.this,R.anim.fadeout);
+                fadeInOnClick[0] = AnimationUtils.loadAnimation(MainActivity.this,R.anim.fadein);
+                themeSong.release();
+                btnExit.startAnimation(fadeOutOnClick[0]);
+                btnExit.startAnimation(fadeInOnClick[0]);
                 finish();
             }
         });
+    }
+
+    public void onDestroy () {
+        //do your stuff here
+        themeSong.release();
+
+        super.onDestroy();
+    }
+
+    private void setAnimationText() {
+        final Animation[] slidetext = new Animation[2];
+        slidetext[0] = AnimationUtils.loadAnimation(this,R.anim.lefttoright);
+        slidetext[1] = AnimationUtils.loadAnimation(this,R.anim.lefttoright);
+
+        slidetext[0].setDuration(600);
+        slidetext[1].setDuration(700);
+
+        text1.setVisibility(View.INVISIBLE);
+        text2.setVisibility(View.INVISIBLE);
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                text1.setVisibility(View.VISIBLE);
+                text1.startAnimation(slidetext[0]);
+            }
+        }, 250);
+
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                text2.setVisibility(View.VISIBLE);
+                text2.startAnimation(slidetext[1]);
+            }
+        }, 500);
+    }
+
+    private void Mapping() {
+        btnStart = findViewById(R.id.BtnPlay);
+        btnHistory = findViewById(R.id.BtnHistory);
+        btnHighscore = findViewById(R.id.BtnHighScore);
+        btnExit = findViewById(R.id.BtnExit);
+        text1 = findViewById(R.id.Txt1);
+        text2 = findViewById(R.id.Txt2);
     }
 }
--- app/src/main/java/com/son/soccertemple/activity_setup.java
+++ app/src/main/java/com/son/soccertemple/activity_setup.java
@@ -2,14 +2,20 @@ package com.son.soccertemple;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
+
 
 import androidx.annotation.Nullable;
 
@@ -21,12 +27,16 @@ public class activity_setup extends Activity {
     Spinner sp;
     Integer Number[] = {5, 6, 7, 8, 9, 10};
     EditText Name;
+    TextView Error;
+    LinearLayout setup;
     int pos;
+    MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setup);
+        mediaPlayer = MediaPlayer.create(this, R.raw.drump);
 
         Mapping();
         createSpin();
@@ -34,10 +44,7 @@ public class activity_setup extends Activity {
         btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(activity_setup.this, MainActivity.class);
-                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                startActivity(intent);
+                finish();
             }
         });
 
@@ -47,34 +54,55 @@ public class activity_setup extends Activity {
                 String userName = Name.getText().toString().trim();
 
                 if(userName.equals("")) {
-                    TextView Error = findViewById(R.id.TxtError);
+                    Error.setText("Vui lòng nhập vào tên người chơi!");
                     Error.setVisibility(View.VISIBLE);
                 }
                 else {
-                    int quizNum = 0;
-                    switch(pos) {
-                        case 0: quizNum = 5; break;
-                        case 1: quizNum = 6; break;
-                        case 2: quizNum = 7; break;
-                        case 3: quizNum = 8; break;
-                        case 4: quizNum = 9; break;
-                        case 5: quizNum = 10; break;
+                    if(userName.length() >= 10) {
+                        Error.setText("Tên không được dài quá 10 ký tự!");
+                        Error.setVisibility(View.VISIBLE);
+                        }
+                    else {
+                        int quizNum = 0;
+
+                        switch (pos) {
+                            case 0:
+                                quizNum = 5;
+                                break;
+                            case 1:
+                                quizNum = 6;
+                                break;
+                            case 2:
+                                quizNum = 7;
+                                break;
+                            case 3:
+                                quizNum = 8;
+                                break;
+                            case 4:
+                                quizNum = 9;
+                                break;
+                            case 5:
+                                quizNum = 10;
+                                break;
+                        }
+
+                        Bundle bundle = new Bundle();
+
+                        bundle.putString("userName", userName);
+                        bundle.putInt("quizNum", quizNum);
+
+                        Intent setDifficulty = new Intent(activity_setup.this, activity_dif.class);
+                        setDifficulty.putExtra("setupPackage", bundle);
+                        startActivity(setDifficulty);
                     }
-
-                    Bundle bundle = new Bundle();
-
-                    bundle.putString("userName", userName);
-                    bundle.putInt("quizNum", quizNum);
-
-                    Intent setDifficulty = new Intent(activity_setup.this, activity_dif.class);
-                    setDifficulty.putExtra("setupPackage", bundle);
-                    startActivity(setDifficulty);
                 }
             }
         });
     }
 
     void Mapping() {
+        setup = findViewById(R.id.layoutSetup);
+        Error = findViewById(R.id.TxtError);
         btnBack = findViewById(R.id.BtnBack);
         btnNext = findViewById(R.id.BtnNext);
         Name = findViewById(R.id.EdtName);
@@ -82,8 +110,23 @@ public class activity_setup extends Activity {
     }
 
     void createSpin() {
-        ArrayAdapter<Integer> adapter = new ArrayAdapter<>
-                (this, android.R.layout.simple_spinner_dropdown_item, Number);
+        ArrayAdapter<Integer> adapter = new ArrayAdapter<Integer>
+                (this, android.R.layout.simple_spinner_dropdown_item, Number){
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                /// Get the Item from ListView
+                View view = super.getView(position, convertView, parent);
+
+                TextView tv = view.findViewById(android.R.id.text1);
+
+                // Set the text size 25 dip for ListView each item
+                tv.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 20);
+                tv.setTextColor(Color.parseColor("#629F6D"));
+
+                // Return the view
+                return view;
+            }
+        };
 
         adapter.setDropDownViewResource(android.R.layout.simple_list_item_single_choice);
         sp.setAdapter(adapter);
@@ -98,5 +141,11 @@ public class activity_setup extends Activity {
                 pos = -1;
             }
         });
+    }
+
+    @Override
+    public void finish() {
+        overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
+        super.finish();
     }
 }
