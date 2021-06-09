d911336afa9adc05e9b79d1f5ecc330cb8f423c9
Dat Nguyen <vghsdat@gmail.com>
2021-06-09 17:21:01 +0700

    Update history: Make descending list

--- app/src/main/java/com/son/soccertemple/activity_history.java
+++ app/src/main/java/com/son/soccertemple/activity_history.java
@@ -47,7 +47,7 @@ public class activity_history extends Activity {
     private void printHistory() {
         ArrayList<String> history = new ArrayList<>();
 
-        for(int i = 0; i < list.size(); i++) {
+        for(int i = list.size() - 1; i >= 0; i--) {
             User user = list.get(i);
             history.add(user.toString());
         }
